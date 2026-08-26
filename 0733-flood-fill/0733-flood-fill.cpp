bf3c50a8201919f93b04a809c0d1a5a0a2088350
Astha  Kriti Raj <astha.raj@s.amity.edu>
2026-08-26 15:05:14 +0530

    Time: 0 ms (100%), Space: 18.6 MB (13.99%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -28,6 +28,51 @@ public:
                     }
                 }
             }
+
+            void bfs(int row, int col,int st_color, int color,
+            vector<vector<int>>& vis, vector<vector<int>>& ans,
+            vector<vector<int>>& original){
+                
+                int n = original.size();
+                int m = original[0].size();
+
+                vis[row][col] = 1;
+                ans[row][col] = color;
+
+                queue<pair<int,int>> q;
+
+                q.push({row, col});
+
+                while(!q.empty()){
+
+                    auto node = q.front(); q.pop();
+
+                    int row = node.first;
+                    int col = node.second;
+
+                    //Find the neighbour
+                    int dr[] = {-1, 1, 0, 0};
+                    int dc[] = {0, 0, 1, -1};
+
+                    for(int i = 0; i < 4; i++){
+                    
+                    int nr = row + dr[i];
+                    int nc = col + dc[i];
+
+                    //validate
+                    if(nr >= 0 && nr < n && nc >= 0 && nc < m){
+                        if(!vis[nr][nc] && original[nr][nc] == st_color){
+                            vis[nr][nc] = 1;
+                            ans[nr][nc] = color;
+                            q.push({nr,nc});
+                        }
+                    }
+                }
+            }
+
+        }
+                
+            
    
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
 
@@ -45,7 +90,8 @@ public:
        }
 
        else{
-        dfs(sr, sc, st_color, color, vis, ans, image);
+        //dfs(sr, sc, st_color, color, vis, ans, image);
+        bfs(sr, sc, st_color, color, vis, ans, image);
        }
 
        return ans;
